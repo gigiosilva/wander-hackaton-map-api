7cb462510f7d12726e20ed10eb0a69c088aedd27
Giovani Silva <giovani.m.silva@gmail.com>
2023-02-07 17:26:43 -0300

    Added Sentry

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 
 import DatabaseConfig from '../ormconfig';
@@ -8,7 +8,12 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BudgetsModule } from './budgets/budgets.module';
 import { BudgetItemsModule } from './budget-items/budget-items.module';
-
+import { SentryInterceptor, SentryModule } from '@ntegral/nestjs-sentry';
+import { APP_INTERCEPTOR, HttpAdapterHost } from '@nestjs/core';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+import { Integration } from "@sentry/types";
+import { ProfilingIntegration } from '@sentry/profiling-node';
 
 @Module({
   imports: [
@@ -23,8 +28,32 @@ import { BudgetItemsModule } from './budget-items/budget-items.module';
     UsersModule,
     BudgetsModule,
     BudgetItemsModule,
+    SentryModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService, HttpAdapterHost],
+      useFactory: async (
+        config: ConfigService,
+        adapterHost: HttpAdapterHost,
+      ) => ({
+        dsn: "https://d559e1feaf024b69b9003f39e69aded9@o4504639496978432.ingest.sentry.io/4504639498551296",
+        integrations: [
+          new Sentry.Integrations.Http({ tracing: true }),
+          new Tracing.Integrations.Express({
+            app: adapterHost.httpAdapter.getInstance(),
+          }),
+          new ProfilingIntegration()
+        ] as Integration[],
+        tracesSampleRate: 1.0,
+        profilesSampleRate: 1.0,
+      }),
+    })
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useFactory: () => new SentryInterceptor(),
+    }
+  ],
 })
 export class AppModule {}
